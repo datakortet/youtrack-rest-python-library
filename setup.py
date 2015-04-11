1b173f2b26535caf82a3bb760c4c9138f13a2b6c
thebjorn <bp@datakortet.no>
2015-04-11 20:31:21 +0200

    Minimal setup.py for importing `youtrack`.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='youtrack',
+    version='1.0.0',
+    packages=['youtrack'],
+    package_dir={
+        '': 'python'
+    },
+    license='Apache 2.0',
+    description='YouTrack rest Python library'
+)
